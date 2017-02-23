96f2dbd333d9f45a772b23c5e616a03d05e9a433
Andy Chu <andychu@viromedia.com>
2017-02-22 16:05:47 -0800

    Add SolarSystemScene to react-viro
    Former-commit-id: d5ff0c891d3394f2b13bb1dd77c7f88cbad69233

--- Examples/index.android.js
+++ Examples/index.android.js
@@ -17,7 +17,7 @@ import {
   ViroSceneNavigator,
 } from 'react-viro';
 
-var InitialScene = require('./js/scene_ios_test');
+var InitialScene = require('./js/SolarSystemScene');
 
 export default class ViroSample extends Component {
   render() {
