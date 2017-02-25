364c56cbdcb9b1e040679746987cb4c15becefcd
Daniel Thian <daniel@viromedia.com>
2017-02-24 16:24:40 -0800

    Added ViroVideoTest
    Former-commit-id: 8bc6b8c6fea2f870f5559f4eb9aa1c64feb44f94

--- Examples/js/release_test/ViroVideoTest.js
+++ Examples/js/release_test/ViroVideoTest.js
@@ -1,93 +1,144 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *//**
-  * Sample React Native App
-  * https://github.com/facebook/react-native
-  */
- 'use strict';
+'use strict';
 
- import React, { Component } from 'react';
- import {
-   AppRegistry,
-   StyleSheet,
-   Text,
-   View
- } from 'react-native';
+import React, { Component } from 'react';
+import {
+  AppRegistry,
+  StyleSheet,
+  Text,
+  View
+} from 'react-native';
 
- import {
-   ViroSceneNavigator,
-   ViroScene,
-   ViroBox,
-   ViroMaterials,
-   ViroNode,
-   ViroOrbitCamera,
-   ViroCamera,
-   ViroAmbientLight,
-   ViroOmniLight,
-   ViroSpotLight,
-   ViroDirectionalLight,
-   ViroImage,
-   ViroVideo,
-   Viro360Image,
-   Viro360Video,
-   ViroFlexView,
-   ViroUtils,
-   ViroText,
-   ViroAnimations,
-   ViroAnimatedComponent,
-   ViroSurface,
-   ViroSkyBox,
-   ViroSphere,
-   Viro3DObject,
- } from 'react-viro';
+import {
+  ViroScene,
+  ViroVideo,
+  ViroOmniLight,
+  ViroSceneNavigator,
+  Materials,
+  ViroText,
+  ViroUtils,
+  Viro360Photo,
+  Viro360Video,
+} from 'react-viro';
 
- let polarToCartesian = ViroUtils.polarToCartesian;
+var VIDEO_REF = "videoref";
+let polarToCartesian = ViroUtils.polarToCartesian;
 
- var ViroTextTest = require('./ViroSurfaceTest');
- var Viro3DObjectTest = require('./Viro3DObjectTest');
+var localVideo = require("./res/Titan_Touchdown.mp4");
 
- // **This test has not been done.  This is placeholder for scene and navigation arrows**
-
- var ViroVideoTest = React.createClass({
-
-   getInitialState() {
-     return {
-
-     };
-   },
-
-   render: function() {
-     return (
+var ViroVideoTest = React.createClass({
+  getInitialState() {
+    return {
+      videoPaused: false,
+      loopVideo: true,
+      muteVideo: true,
+      videoSource: {uri:'https://s3.amazonaws.com/viro.video/Climber2Top.mp4'},
+      volume: 1,
+      showVideo: true,
+    }
+  },
+  render: function() {
+    return (
       <ViroScene>
-      <ViroOmniLight position={[0, 0, 0]} color="#ffffff" attenuationStartDistance={40} attenuationEndDistance={50}/>
+        <ViroOmniLight position={[0, 0, 0]} color="#ffffff"
+          attenuationStartDistance={40} attenuationEndDistance={50} />
+ {this._getViroVideo()}
 
-      <Viro360Image
-       rotation={[0,0,0]}
-       source={require("./res/360_park.jpg")}
-       />
+<ViroText position={polarToCartesian([2, -10, 0])} text={this._getLoopingText()}
+          style={styles.baseTextTwo} onClick={this._toggleLooping} transformBehaviors={["billboard"]}/>
+        <ViroText position={polarToCartesian([2, -10, -5])} text="Restart"
+          style={styles.baseTextTwo} onClick={this._restartVideo} transformBehaviors={["billboard"]}/>
+        <ViroText position={polarToCartesian([2, -10, -10])} text="Toggle Pause"
+          style={styles.baseTextTwo} onClick={this._togglePauseVideo} transformBehaviors={["billboard"]}/>
+        <ViroText position={polarToCartesian([2, -10, -15])} text={this._getMuteText()}
+          style={styles.baseTextTwo} onClick={this._muteVideo} transformBehaviors={["billboard"]}/>
+        <ViroText position={polarToCartesian([2, -10, -20])} text="Change Source"
+          style={styles.baseTextTwo} onClick={this._changeVideoSource} transformBehaviors={["billboard"]}/>
+        <ViroText position={polarToCartesian([2, 15, 0])} text={"Change Volume from: " + this.state.volume}
+          style={styles.baseTextTwo} onClick={this._changeVolume} transformBehaviors={["billboard"]}/>
+          </ViroScene>
+    );
+  },
+  _getViroVideo() {
+    if (this.state.showVideo) {
+      return (
+        <ViroVideo ref={VIDEO_REF}  width={1.7} height={0.95} position={[0, 1, -2.9]} scale={[1, 1, 1]} paused={this.state.videoPaused}
+          source={localVideoc} transformBehavior={["billboard"]}
+          loop={this.state.loopVideo} muted={this.state.muteVideo} volume={this.state.volume}
+          onFinish={this._onVideoFinished} onUpdateTime={this._onUpdateTime}/>
+      );
+    } else {
+      return; (
+        <ViroVideo ref={VIDEO_REF} width={1.7} height={0.95} position={[0, 1, -2.9]} scale={[1, 1, 1]} paused={this.state.videoPaused}
+          source={{uri:'https://s3.amazonaws.com/viro.video/Climber1Bottom.mp4'}} transformBehavior={["billboard"]}
+          loop={this.state.loopVideo} muted={this.state.muteVideo} volume={this.state.volume}
+          onFinish={this._onVideoFinished} onUpdateTime={this._onUpdateTime}/>
+      );
+    }
+  },
+  _onVideoFinished(){
+    console.log("Viro on video Finished");
+  },
+  _onUpdateTime(current, total){
+    console.log("Viro On time update-> Current: " + current+ ", total: " + total);
+  },
+  _changeVideoSource() {
+    this.setState({
+      videoSource: {uri:'https://s3.amazonaws.com/viro.video/Climber1Bottom.mp4'},
+      showVideo: !this.state.showVideo,
+    })
+  },
+  _getMuteText() {
+    return this.state.muteVideo ? "Unmute" : "Mute";
+  },
+  _muteVideo() {
+    this.setState({
+      muteVideo: !this.state.muteVideo,
+    })
+  },
+  _togglePauseVideo() {
+    this.setState({
+      videoPaused: !this.state.videoPaused,
+    })
+  },
+  _getLoopingText() {
+    return this.state.loopVideo ? "Disable Looping" : "Enable Looping";
+  },
+  _toggleLooping() {
+    this.setState({
+      loopVideo: !this.state.loopVideo,
+    });
+  },
+  _pauseVideo() {
+    console.log("pausing video!");
+    this.setState({
+      videoPaused: true,
+    })
+  },
+  _playVideo() {
+    console.log("playing video!");
+    this.setState({
+      videoPaused: false,
+    })
+  },
+  _changeVolume() {
+    this.setState({
+      volume: Math.random(),
+    });
+  },
+  _restartVideo() {
+    this.refs[VIDEO_REF].seekToTime(0);
+  },
+});
 
+var styles = StyleSheet.create({
+  baseTextTwo: {
+      fontFamily: 'Arial',
+      fontSize: 10,
+      color: '#ffffff',
+      flex: 1,
+      textAlignVertical: 'center',
+      textAlign: 'center',
+  },
+});
 
-       <ViroImage source={require('./res/icon_left_w.png')} position={[-2, -4, -3]} scale={[1, 1, 1]} transformBehaviors={["billboard"]} onClick={this._showPrevious} />
-       <ViroText text="ViroVideo" position={[0, -5, -3]} transformBehaviors={["billboard"]} />
-       <ViroImage source={require('./res/icon_right_w.png')} position={[2, -4, -3]} scale={[1, 1, 1]} transformBehaviors={["billboard"]} onClick={this._showNext} />
-
-
-      </ViroScene>
-
-     );
-   },
-
-   _showPrevious() {
-     this.props.sceneNavigator.pop();
-   },
-
-   _showNext() {
-     this.props.sceneNavigator.push({scene:Viro3DObjectTest});
-   },
-
-
- });
-
-
- module.exports = ViroVideoTest;
+module.exports = ViroVideoTest;
