6521737d088e918aad2f1e92e31a72802ba9491b
Vik Advani <vadvani@viromedia.com>
2017-02-28 00:40:17 -0800

    VIRO-784 - Create react-viro cli scripts for iOS
    Filled in ios-setup script which does the following:
    
    - Copies ngrok bash script files to ios directory of new project.
    - Copies podFile to ios dir of new project.
    - Modifies templated podFile with project name.
    - Modifies templated AppDelegate.m with proper project name
    - Executes pod install.
    - Executes ruby script which uses xcodeproj to add shell build steps for ngrok
      and modified build settings for bitcode.
    - Executes PlistBuddy to modify info.plist
    
    
    Former-commit-id: 050b22b6b3fdae4bc6076e996c8b191f66235c06

--- bin/ios-setup.sh
+++ bin/ios-setup.sh
@@ -9,9 +9,38 @@
 VIRO_PROJECT_NAME=$1 # The name of the user's project. The cwd should be already here.
 VIRO_VERBOSE=$2 # True/False whether or not the user ran init w/ --verbose option
 
-if [ "$VIRO_VERBOSE" = true ]; then 
+if [ "$VIRO_VERBOSE" = true ]; then
   echo "running with verbose logging"
 fi
 
-echo "ios-setup.sh - doing nothing"
+currentDir=pwd
+echo $currentDir
 
+cp node_modules/react-viro/ios/dist/podFile/* ios/
+cp node_modules/react-viro/ios/dist/bin/* ios/
+chmod a+x ios/ngrok_launch.sh
+chmod a+x ios/ngrok_term_launch.command
+
+#replace Podfile with proper project name
+TARGET_FILEPATH=ios/Podfile
+LINE_TO_ADD="$VIRO_PROJECT_NAME"
+SEARCH_PATTERN="VIRO_PROJECT_NAME"
+LINE_TO_REPLACE=$(grep "$SEARCH_PATTERN" "$TARGET_FILEPATH")
+sed -i '' "s/$SEARCH_PATTERN/$LINE_TO_ADD/g" $TARGET_FILEPATH
+
+cd ios
+#install the pod for ios
+pod install
+
+#run xcodeproj script to modify ios project settings.
+ruby proj_add_shell_script.rb "$VIRO_PROJECT_NAME"
+
+#modify info.plist file using PlistBuddy(installed by xcode)
+ /usr/libexec/PlistBuddy -c "Add:NSAppTransportSecurity:NSAllowsArbitraryLoads bool true" $VIRO_PROJECT_NAME/Info.plist
+
+yes | cp -rf ../node_modules/react-viro/ios/dist/AppDelegate.m $VIRO_PROJECT_NAME/AppDelegate.m
+TARGET_FILEPATH=$VIRO_PROJECT_NAME/AppDelegate.m
+LINE_TO_ADD="$VIRO_PROJECT_NAME"
+SEARCH_PATTERN="VIRO_PROJECT_NAME"
+LINE_TO_REPLACE=$(grep "$SEARCH_PATTERN" "$TARGET_FILEPATH")
+sed -i '' "s/$SEARCH_PATTERN/$LINE_TO_ADD/g" $TARGET_FILEPATH
