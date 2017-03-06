0c5503fb3c87b5ebea473e94d25979451e792aa9
Raj Advani <radvani@viromedia.com>
2017-03-06 18:33:38 -0500

    VIRO-716: allow material remote downloads
    Materials now support a URI for downloads.
    However, the material manager still downloads
    these images on the *main thread* so this is
    is highly discouraged. Created a post-beta story
    VIRO-912 to refactor both the Android and iOS
    material managers to download images async.
    
    
    Former-commit-id: 448c223bfa63d97cbb69bd18066e130e7a34dd85

--- ios/ViroReact/Modules/VRTMaterialManager.mm
+++ ios/ViroReact/Modules/VRTMaterialManager.mm
@@ -9,6 +9,8 @@
 #import <React/RCTConvert.h>
 #import "VRTMaterialManager.h"
 #import "VRTImage.h"
+#import <React/RCTUtils.h>
+#import <React/RCTImageSource.h>
 
 @implementation RCTBridge (VRTMaterialManager)
 
@@ -167,8 +169,8 @@ RCT_EXPORT_METHOD(setJSMaterials:(NSDictionary *)materialsDict)
   NSString *path = [self parseImagePath:json];
   
   UIImage *image = _imageDictionary[path];
-  if(image == nil){
-    image = [RCTConvert UIImage:json];
+  if (!image) {
+    image = [self downloadImageSync:json];
     if (!image) {
       image = [RCTConvert UIImage:[json objectForKey:@"source"]];
     }
@@ -406,6 +408,80 @@ RCT_EXPORT_METHOD(setJSMaterials:(NSDictionary *)materialsDict)
     return [materialWrapper getVideoTextures];
   }
   return nil;
+}
+
+// DEPRECATED: this is only in place for Beta. This needs to be
+//             replaced with asynchronous image loading.
+//             Tracked by VIRO-912
+- (UIImage *)downloadImageSync:(id)json
+{
+  if (!json) {
+    return nil;
+  }
+  
+  RCTImageSource *imageSource = [RCTConvert RCTImageSource:json];
+  if (!imageSource) {
+    return nil;
+  }
+  
+  __block UIImage *image;
+  if (!RCTIsMainQueue()) {
+    // It seems that none of the UIImage loading methods can be guaranteed
+    // thread safe, so we'll pick the lesser of two evils here and block rather
+    // than run the risk of crashing
+    RCTLogWarn(@"Calling [RCTConvert UIImage:] on a background thread is not recommended");
+    dispatch_sync(dispatch_get_main_queue(), ^{
+      image = [RCTConvert UIImage:json];
+    });
+    return image;
+  }
+  
+  NSURL *URL = imageSource.request.URL;
+  NSString *scheme = URL.scheme.lowercaseString;
+  if ([scheme isEqualToString:@"file"]) {
+    NSString *assetName = RCTBundlePathForURL(URL);
+    image = assetName ? [UIImage imageNamed:assetName] : nil;
+    if (!image) {
+      // Attempt to load from the file system
+      NSString *filePath = URL.path;
+      if (filePath.pathExtension.length == 0) {
+        filePath = [filePath stringByAppendingPathExtension:@"png"];
+      }
+      image = [UIImage imageWithContentsOfFile:filePath];
+      if (!image) {
+        RCTLogConvertError(json, @"an image. File not found.");
+      }
+    }
+  } else if ([scheme isEqualToString:@"data"]) {
+    image = [UIImage imageWithData:[NSData dataWithContentsOfURL:URL]];
+  } else if ([scheme hasPrefix:@"http"]) {
+    image = [UIImage imageWithData:[NSData dataWithContentsOfURL:URL]];
+  } else {
+    RCTLogConvertError(json, @"an image. Only local files or data URIs are supported.");
+    return nil;
+  }
+  
+  CGFloat scale = imageSource.scale;
+  if (!scale && imageSource.size.width) {
+    // If no scale provided, set scale to image width / source width
+    scale = CGImageGetWidth(image.CGImage) / imageSource.size.width;
+  }
+  
+  if (scale) {
+    image = [UIImage imageWithCGImage:image.CGImage
+                                scale:scale
+                          orientation:image.imageOrientation];
+  }
+  
+  if (!CGSizeEqualToSize(imageSource.size, CGSizeZero) &&
+      !CGSizeEqualToSize(imageSource.size, image.size)) {
+    RCTLogError(@"Image source %@ size %@ does not match loaded image size %@.",
+                URL.path.lastPathComponent,
+                NSStringFromCGSize(imageSource.size),
+                NSStringFromCGSize(image.size));
+  }
+  
+  return image;
 }
 
 @end
