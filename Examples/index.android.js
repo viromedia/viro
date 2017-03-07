d45eec34fbd18e24412f444aaae52f6be723be23
Manish Bodhankar <manish@viromedia.com>
2017-03-07 17:49:55 -0600

    Add photo viewer home scene
    Former-commit-id: 0a99ad0b36b98222b0868a86132a22ce613366a2

--- Examples/index.android.js
+++ Examples/index.android.js
@@ -17,7 +17,7 @@ import {
   ViroSceneNavigator,
 } from 'react-viro';
 
-var InitialScene = require('./js/whackamole/MainMenu');
+var InitialScene = require('./js/photo_viewer/HomeMenuScene');
 
 export default class ViroSample extends Component {
   render() {
