b0a09ab2baab873c22fe7c76d2926dca4e5d1f65
Raj Advani <radvani@viromedia.com>
2017-03-19 22:16:44 -0400

    Make light test more clear
    Add comments and make colors of the
    spotlights different so can differentiate.
    
    
    Former-commit-id: a57acfcc988efa923660eecb12d6b9594ac2d847

--- Examples/js/release_test/ViroLightTest.js
+++ Examples/js/release_test/ViroLightTest.js
@@ -51,11 +51,12 @@
 
    getInitialState() {
      return {
-        lightPosHeight:6,
+        lightPosHeight:4,
         lightAttenuationDistanceStart:3.5,
         lightInnerAngle:5,
         lightOuterAngle:30,
-        lightColor:"#ff8888",
+        lightColor0:"#ff8888",
+        lightColor1:"#88ff88",
         sceneColor:"#ffffff",
         lightDirection:[0,-1,0],
         showAbientLight:false,
@@ -64,37 +65,33 @@
    },
 
    _renderDirectionalLight(){
-    if (!this.state.showDirectionalLight){
-        return;
-    }
-    return(
-       <ViroDirectionalLight
-                direction={[0,0,-1]}
-                color={this.state.sceneColor}/>
-    )
+      if (!this.state.showDirectionalLight){
+          return;
+      }
+      return(
+         <ViroDirectionalLight direction={[0, 0, -1]} color={this.state.sceneColor}/>
+      )
    },
-
    _renderAmbientLight(){
-   console.log("Scene color is: " + this.state.sceneColor);
-
        if (!this.state.showAbientLight){
            return;
        }
-   return(
-        <ViroAmbientLight
-            color={this.state.sceneColor}/>
+       return(
+          <ViroAmbientLight color={this.state.sceneColor}/>
        )
    },
+
    _toggleColorLights(){
-        var newLightColor = "#ff8888";
-        if (this.state.lightColor == "#ff8888"){
-            newLightColor = "#8888ff";
-        }
-
-
-         this.setState({
-                 lightColor: newLightColor,
-               });
+     var newLightColor0 = "#ff8888";
+     var newLightColor1 = "#88ff88";
+     if (this.state.lightColor0 == "#ff8888"){
+         newLightColor0 = "#88ff88";
+         newLightColor1 = "#ff8888"
+     }
+     this.setState({
+       lightColor0: newLightColor0,
+       lightColor1: newLightColor1,
+     });
    },
    _toggleColorScene(){
         var newSceneColor = "#ffffff";
@@ -105,84 +102,76 @@
                  sceneColor: newSceneColor
                });
    },
-
    _toggleSpotLightPosition(){
-       var newlightPosHeight = this.state.lightPosHeight + .5;
-       if (newlightPosHeight > 8){
-           newlightPosHeight = 4;
-       }
-         this.setState({
-                 lightPosHeight: newlightPosHeight,
-               });
+     var newlightPosHeight = this.state.lightPosHeight + .5;
+     if (newlightPosHeight > 8){
+         newlightPosHeight = 4;
+     }
+     this.setState({
+       lightPosHeight: newlightPosHeight,
+      });
    },
    _toggleSpotLightAttenuation(){
-          var newlightAttenuationDistanceStart = this.state.lightAttenuationDistanceStart + .5;
-          if (newlightAttenuationDistanceStart > 6){
-              newlightAttenuationDistanceStart = 1;
-          }
-            this.setState({
-                    lightAttenuationDistanceStart: newlightAttenuationDistanceStart,
-                  });
+      var newlightAttenuationDistanceStart = this.state.lightAttenuationDistanceStart + .5;
+      if (newlightAttenuationDistanceStart > 6){
+          newlightAttenuationDistanceStart = 1;
+      }
+      this.setState({
+        lightAttenuationDistanceStart: newlightAttenuationDistanceStart,
+      });
    },
    _toggleSpotLightInnerAngle(){
        var newlightInnerAngle = this.state.lightInnerAngle + 5;
        if (newlightInnerAngle > 30){
            newlightInnerAngle = 5;
        }
-         this.setState({
-                 lightInnerAngle: newlightInnerAngle,
-               });
+       this.setState({
+         lightInnerAngle: newlightInnerAngle,
+       });
    },
    _toggleSpotLightOuterAngle(){
-    var newlightOuterAngle = this.state.lightOuterAngle + 5;
-    if (newlightOuterAngle > 40){
-        newlightOuterAngle = 25;
-    }
-      this.setState({
-              lightOuterAngle: newlightOuterAngle,
-            });
-   },
+       var newlightOuterAngle = this.state.lightOuterAngle + 5;
+       if (newlightOuterAngle > 40){
+         newlightOuterAngle = 5;
+       }
 
+       this.setState({
+         lightOuterAngle: newlightOuterAngle,
+       });
+   },
    _toggleAmbientLight(){
-   this.setState({
-        showAbientLight:!this.state.showAbientLight
-               });
+       this.setState({
+         showAbientLight:!this.state.showAbientLight
+       });
    },
    _toggleDirectionalLight(){
-   this.setState({
-        showDirectionalLight:!this.state.showDirectionalLight
-               });
+       this.setState({
+         showDirectionalLight:!this.state.showDirectionalLight
+       });
    },
 
    _renderLightControls(){
-    return(
-    <ViroNode position={[-0,1,-6]}>
-    <ViroText fontSize={this.state.fontSize} style={styles.centeredText} position={[-2, 1, 0]} width={2} height ={2} fontFamily={'Arial'}
-        text={"Toggle Color Scene"} textLineBreakMode='justify' onClick={this._toggleColorScene}/>
-
-    <ViroText fontSize={this.state.fontSize} style={styles.centeredText} position={[0,1, 0]} width={2} height ={2} fontFamily={'Arial'}
-        text={"Toggle Spot / Omni Height " + this.state.lightPosHeight} textLineBreakMode='justify' onClick={this._toggleSpotLightPosition}/>
-
-    <ViroText fontSize={this.state.fontSize} style={styles.centeredText} position={[2, 1, 0]} width={2} height ={2} fontFamily={'Arial'}
-        text={"Toggle Spot lightAttenuation  " + this.state.lightAttenuationDistanceStart + " - " + (this.state.lightAttenuationDistanceStart + 3)} textLineBreakMode='justify' onClick={this._toggleSpotLightAttenuation}/>
-
-    <ViroText fontSize={this.state.fontSize} style={styles.centeredText} position={[-2,0, 0]} width={2} height ={2} fontFamily={'Arial'}
-        text={"Toggle Spot Inner Angle " + this.state.lightInnerAngle} textLineBreakMode='justify' onClick={this._toggleSpotLightInnerAngle}/>
-
-    <ViroText fontSize={this.state.fontSize} style={styles.centeredText} position={[0, 0, 0]} width={2} height ={2} fontFamily={'Arial'}
-        text={"Toggle Spot Outer Angle " + this.state.lightOuterAngle} textLineBreakMode='justify' onClick={this._toggleSpotLightOuterAngle}/>
-
-    <ViroText fontSize={this.state.fontSize} style={styles.centeredText} position={[2, 0, 0]} width={2} height ={2} fontFamily={'Arial'}
-            text={"Toggle Ambient Light " + this.state.showAbientLight} textLineBreakMode='justify' onClick={this._toggleAmbientLight}/>
-        <ViroText fontSize={this.state.fontSize} style={styles.centeredText} position={[0, -1, 0]} width={1.5} height ={2} fontFamily={'Arial'}
-                text={"Toggle Color Spot & Omni light"} textLineBreakMode='justify' onClick={this._toggleColorLights}/>
-        <ViroText fontSize={this.state.fontSize} style={styles.centeredText} position={[2, -1, 0]} width={2} height ={2} fontFamily={'Arial'}
-                text={"Toggle Directional Light " + this.state.showDirectionalLight} textLineBreakMode='justify' onClick={this._toggleDirectionalLight}/>
+     return(
+        <ViroNode position={[-0,1,-6]}>
+          <ViroText fontSize={this.state.fontSize} style={styles.centeredText} position={[-2, 1, 0]} width={2} height ={2} fontFamily={'Arial'}
+              text={"Toggle Color Scene"} textLineBreakMode='justify' onClick={this._toggleColorScene}/>
+          <ViroText fontSize={this.state.fontSize} style={styles.centeredText} position={[0,1, 0]} width={2} height ={2} fontFamily={'Arial'}
+              text={"Toggle Spot / Omni Height " + this.state.lightPosHeight} textLineBreakMode='justify' onClick={this._toggleSpotLightPosition}/>
+          <ViroText fontSize={this.state.fontSize} style={styles.centeredText} position={[2, 1, 0]} width={2} height ={2} fontFamily={'Arial'}
+              text={"Toggle Spot lightAttenuation  " + this.state.lightAttenuationDistanceStart + " - " + (this.state.lightAttenuationDistanceStart + 3)} textLineBreakMode='justify' onClick={this._toggleSpotLightAttenuation}/>
+          <ViroText fontSize={this.state.fontSize} style={styles.centeredText} position={[-2,0, 0]} width={2} height ={2} fontFamily={'Arial'}
+              text={"Toggle Spot Inner Angle " + this.state.lightInnerAngle} textLineBreakMode='justify' onClick={this._toggleSpotLightInnerAngle}/>
+          <ViroText fontSize={this.state.fontSize} style={styles.centeredText} position={[0, 0, 0]} width={2} height ={2} fontFamily={'Arial'}
+              text={"Toggle Spot Outer Angle " + this.state.lightOuterAngle} textLineBreakMode='justify' onClick={this._toggleSpotLightOuterAngle}/>
+          <ViroText fontSize={this.state.fontSize} style={styles.centeredText} position={[2, 0, 0]} width={2} height ={2} fontFamily={'Arial'}
+              text={"Toggle Ambient Light " + this.state.showAbientLight} textLineBreakMode='justify' onClick={this._toggleAmbientLight}/>
+          <ViroText fontSize={this.state.fontSize} style={styles.centeredText} position={[0, -1, 0]} width={1.5} height ={2} fontFamily={'Arial'}
+              text={"Toggle Color Spot & Omni light"} textLineBreakMode='justify' onClick={this._toggleColorLights}/>
+          <ViroText fontSize={this.state.fontSize} style={styles.centeredText} position={[2, -1, 0]} width={2} height ={2} fontFamily={'Arial'}
+              text={"Toggle Directional Light " + this.state.showDirectionalLight} textLineBreakMode='justify' onClick={this._toggleDirectionalLight}/>
         </ViroNode>
-    );
+      );
    },
-
-
 
    render: function() {
      return (
@@ -190,60 +179,88 @@
           {this._renderAmbientLight()}
           {this._renderDirectionalLight()}
           {this._renderLightControls()}
+
+          {
+           /*
+            The box to the far left, has a Constant lighting material so should
+            be unaffected by any light.
+            */
+          }
           <ViroBox
             materials={["whiteColorConst"]}
-            position={[-9,-2,-5]}
-            scale={[1 , 1 , 1]}
+            position={[-9, -2, -5]}
+            scale={[1, 1, 1]}
             height={2}
             width={2}
             length={2}/>
 
+            {
+             /*
+              The box to the immediate left. This box is meant to test the spotlight.
+              It is also lit by the global ambient and directional lights.
+              */
+            }
           <ViroNode position={[-5,-2,-7]}>
               <ViroSpotLight
                   innerAngle={this.state.lightInnerAngle}
                   outerAngle={this.state.lightOuterAngle}
                   direction={this.state.lightDirection}
                   position={[0, this.state.lightPosHeight, 0]}
-                  color={this.state.lightColor}
+                  color={this.state.lightColor1}
                   attenuationStartDistance={this.state.lightAttenuationDistanceStart}
                   attenuationEndDistance={this.state.lightAttenuationDistanceStart + 3}/>
 
               <ViroBox
                 materials={["whiteColorBlinn"]}
-                position={[0 , 0, 0]}
-                scale={[1 , 1 , 1]}
+                position={[0, 0, 0]}
+                scale={[1, 1, 1]}
                 height={2}
                 width={3}
                 length={3}/>
-<ViroText fontSize={this.state.fontSize} style={styles.centeredText} position={[2,-1, 2]} width={2} height ={2} fontFamily={'Arial'}
-   text={"Boxes are position in y at 0"} textLineBreakMode='justify'/>
-
           </ViroNode>
+
+          {
+           /*
+            The large box in the middle, lit by an omni light and the ambient +
+            directional global lights.
+            */
+          }
           <ViroNode position={[-1,-2,-7]}>
               <ViroOmniLight
                   position={[0, this.state.lightPosHeight, 0]}
-                  color={this.state.lightColor}
+                  color={this.state.lightColor0}
                   attenuationStartDistance={this.state.lightAttenuationDistanceStart}
                   attenuationEndDistance={this.state.lightAttenuationDistanceStart + 3} />
 
               <ViroBox
                 materials={["whiteColorLambert"]}
-                position={[0 , 0, 0]}
-                scale={[1 , 1 , 1]}
+                position={[0, 0, 0]}
+                scale={[1, 1, 1]}
                 height={2}
                 width={3}
                 length={3}/>
-
           </ViroNode>
 
-          <ViroNode position={[4,-2,-7]}>
+          {
+           /*
+            These are the 3 boxes to the right of the viewer. They are lit by
+            the omni light below, and the global ambient/directional lights.
+            */
+          }
+          <ViroNode position={[4, -2, -7]}>
+              {
+               /*
+                The spotlight within this node currently has no effect, because
+                lights only impact their child nodes.
+                */
+              }
               <ViroNode position={[0, 5, 0]}>
                 <ViroSpotLight
                     position={[0, 0, 0]}
                     innerAngle={this.state.lightInnerAngle}
                     outerAngle={this.state.lightOuterAngle}
                     direction={this.state.lightDirection}
-                    color={this.state.lightColor}
+                    color={"#8888FF"}
                     attenuationStartDistance={3.5}
                     attenuationEndDistance={5}/>
               </ViroNode>
@@ -255,52 +272,37 @@
 
               <ViroBox
                     materials={["whiteColorLambert"]}
-                    position={[-1.2 , 0, 0]}
-                    scale={[1 , 1 , 1]}
+                    position={[-1.2, 0, 0]}
+                    scale={[1, 1, 1]}
                     height={1}
                     width={1}
                     length={1}/>
               <ViroBox
                     materials={["whiteColorLambert"]}
-                    position={[0 , 0, 0]}
-                    scale={[1 , 1 , 1]}
+                    position={[0, 0, 0]}
+                    scale={[1, 1, 1]}
                     height={1}
                     width={1}
                     length={1}/>
               <ViroBox
                     materials={["whiteColorLambert"]}
-                    position={[1.2 , 0, 0]}
-                    scale={[1 , 1 , 1]}
+                    position={[1.2, 0, 0]}
+                    scale={[1, 1, 1]}
                     height={1}
                     width={1}
                     length={1}/>
          </ViroNode>
       </ViroScene>
-
      );
    },
  });
 
 const styles = StyleSheet.create({
-  containerVertical: {
-    flexDirection: 'column',
-  },
-    containerHorizontal: {
-      flexDirection: 'row',
-    },
-
    centeredText: {
         fontSize: 20,
         fontFamily: 'Arial',
         color: '#ffffff',
    },
-});
-ViroAnimations.registerAnimations({
-    moveRight:{properties:{positionX:"+1"}, duration: 2000, easing:"Linear"},
-    moveLeft:{properties:{positionX:"+-1"}, duration: 2000, easing:"Linear"},
-    testLoopMove:[
-        ["moveRight", "moveLeft"]
-    ],
 });
 
 ViroMaterials.createMaterials({
@@ -313,10 +315,6 @@ ViroMaterials.createMaterials({
   },
   whiteColorBlinn: {
     lightingModel: "Blinn",
-    diffuseColor: "#ffffff"
-  },
-  whiteColorPhong: {
-    lightingModel: "Phong",
     diffuseColor: "#ffffff"
   },
  });
