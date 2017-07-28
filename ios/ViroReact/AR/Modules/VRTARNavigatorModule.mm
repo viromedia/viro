0abc057a207d1452a04d7ee2c7ad039d65d73572
Andy Chu <andychu@viromedia.com>
2017-07-28 15:28:18 -0700

    Remove debug logs
    Former-commit-id: 2321c1c69d10b082e08be76e744123913b791c7f

--- ios/ViroReact/AR/Modules/VRTARNavigatorModule.mm
+++ ios/ViroReact/AR/Modules/VRTARNavigatorModule.mm
@@ -20,16 +20,14 @@ RCT_EXPORT_MODULE()
 }
 
 
-RCT_EXPORT_METHOD(startVideoRecording:(nonnull NSNumber *)reactTag fileName:(NSString *)fileName saveToCameraRoll:(BOOL)saveToCameraRoll) {
-    NSLog(@"kirby startVideoRecording 1");
+RCT_EXPORT_METHOD(startVideoRecording:(nonnull NSNumber *)reactTag
+                             fileName:(NSString *)fileName
+                     saveToCameraRoll:(BOOL)saveToCameraRoll) {
     [self.bridge.uiManager addUIBlock:^(__unused RCTUIManager *uiManager, NSDictionary<NSNumber *, UIView *> *viewRegistry) {
-        NSLog(@"kirby startVideoRecording 2");
         VRTView *view = (VRTView *)viewRegistry[reactTag];
         if (![view isKindOfClass:[VRTARNavigator class]]) {
-            NSLog(@"kirby startVideoRecording 3");
             RCTLogError(@"Invalid view returned from registry, expecting VRTARNavigator, got: %@", view);
         } else {
-            NSLog(@"kirby startVideoRecording 4");
             VRTARNavigator *component = (VRTARNavigator *)view;
             [component startVideoRecording:fileName saveToCameraRoll:saveToCameraRoll];
         }
@@ -39,18 +37,13 @@ RCT_EXPORT_METHOD(startVideoRecording:(nonnull NSNumber *)reactTag fileName:(NSS
 RCT_EXPORT_METHOD(stopVideoRecording:(nonnull NSNumber *)reactTag
                   resolve:(RCTPromiseResolveBlock)resolve
                   reject:(RCTPromiseRejectBlock)reject) {
-    NSLog(@"kirby stopVideoRecording 1");
     [self.bridge.uiManager addUIBlock:^(__unused RCTUIManager *uiManager, NSDictionary<NSNumber *, UIView *> *viewRegistry) {
-        NSLog(@"kirby stopVideoRecording 2");
         VRTView *view = (VRTView *)viewRegistry[reactTag];
         if (![view isKindOfClass:[VRTARNavigator class]]) {
-            NSLog(@"kirby stopVideoRecording 3");
             RCTLogError(@"Invalid view returned from registry, expecting VRTARNavigator, got: %@", view);
         } else {
-            NSLog(@"kirby stopVideoRecording 4");
             VRTARNavigator *component = (VRTARNavigator *)view;
             [component stopVideoRecordingWithHandler:^(NSURL *url) {
-                NSLog(@"kirby stopVideoRecording 5");
                 resolve([url path]);
             }];
         }
