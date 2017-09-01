ea83597536648906bd1c52ef461101712afc79e9
Daniel Thian <daniel@viromedia.com>
2017-09-01 16:08:18 -0700

    Updated Firework Particles
    Former-commit-id: 4b95c2d2531ddfece078ace89a26995c99a4ef6c

--- Figment/js/effects/effects.js
+++ Figment/js/effects/effects.js
@@ -165,32 +165,32 @@
     var f = new Array(30);
     f[1] = getFireWork(polarToCartesian([radius, -90 , 30]), colors[1], 1000);
     f[2] = getFireWork(polarToCartesian([radius, -70 , 20]), colors[1], 2000);
-    f[3] = getFireWork(polarToCartesian([radius, -50 , 40]), colors[2], 3000);
+    f[3] = getFireWork(polarToCartesian([radius, -50 , 32]), colors[2], 3000);
     f[4] = getFireWork(polarToCartesian([radius, -30 , 28]), colors[4], 4000);
-    f[5] = getFireWork(polarToCartesian([radius, -10 , 25]), colors[5], 4500);
-    f[6] = getFireWork(polarToCartesian([radius, 10 , 20]), colors[7], 5000);
+    f[9] = getFireWork(polarToCartesian([radius, -15 , 20]), colors[5], 2000);
+    f[5] = getFireWork(polarToCartesian([radius, 10 , 24]), colors[5], 4500);
+    f[6] = getFireWork(polarToCartesian([radius, 20 , 20]), colors[7], 3000);
     f[7] = getFireWork(polarToCartesian([radius, 30 , 40]), colors[7], 4000);
     f[8] = getFireWork(polarToCartesian([radius, 50 , 35]), colors[2], 3000);
-    f[9] = getFireWork(polarToCartesian([radius, 70 , 25]), colors[5], 2000);
-    f[10] = getFireWork(polarToCartesian([radius, 80 , 10]), colors[5], 1000);
+    f[10] = getFireWork(polarToCartesian([radius, 80 , 10]), colors[5], 4500);
 
     f[11] = getFireWorkGravity(polarToCartesian([radius, -85 , 20]), colors[7], 3000);
-    f[12] = getFireWorkGravity(polarToCartesian([radius, -60 , 45]), colors[0], 1500);
-    f[13] = getFireWorkGravity(polarToCartesian([radius, -40 , 40]), colors[3], 2500);
-    f[14] = getFireWorkGravity(polarToCartesian([radius, -20 , 25]), colors[5], 1500);
-    f[15] = getFireWorkGravity(polarToCartesian([radius, -5 , 30]), colors[6], 500);
-    f[29] = getFireWorkGravity(polarToCartesian([radius, 5 , 25]), colors[6], 4500);
-    f[16] = getFireWorkGravity(polarToCartesian([radius, 20 , 20]), colors[3], 1500);
-    f[17] = getFireWorkGravity(polarToCartesian([radius, 40 , 40]), colors[3], 2500);
-    f[18] = getFireWorkGravity(polarToCartesian([radius, 60 , 25]), colors[3], 1500);
+    f[12] = getFireWorkGravity(polarToCartesian([radius, -60 , 42]), colors[0], 4800);
+    f[13] = getFireWorkGravity(polarToCartesian([radius, -40 , 10]), colors[3], 2600);
+    f[14] = getFireWorkGravity(polarToCartesian([radius, -10 , 15]), colors[5], 1500);
+    f[16] = getFireWorkGravity(polarToCartesian([radius, -5 , 20]), colors[3], 300);
+    f[15] = getFireWorkGravity(polarToCartesian([radius, 5 , 20]), colors[6], 3000);
+    f[29] = getFireWorkGravity(polarToCartesian([radius, 12 , 15]), colors[6], 1300);
+    f[17] = getFireWorkGravity(polarToCartesian([radius, 30 , 40]), colors[3], 2200);
+    f[18] = getFireWorkGravity(polarToCartesian([radius, 45 , 25]), colors[3], 1500);
     f[19] = getFireWorkGravity(polarToCartesian([radius, 85 , 36]), colors[3], 3500);
 
     f[20] = getFireWorkBrightDarkBright(polarToCartesian([radius, -80 , 30]), colors[0], colors[3], 2340);
     f[21] = getFireWorkBrightDarkBright(polarToCartesian([radius, -50 , 40]), colors[1], colors[4], 0);
     f[22] = getFireWorkBrightDarkBright(polarToCartesian([radius, -25 , 20]), colors[2], colors[5], 800);
     f[23] = getFireWorkBrightDarkBright(polarToCartesian([radius, -15 , 33]), colors[3], colors[7], 2000);
-    f[24] = getFireWorkBrightDarkBright(polarToCartesian([radius, 8 , 40]), colors[4], colors[6], 1900);
-    f[25] = getFireWorkBrightDarkBright(polarToCartesian([radius, 15 , 20]), colors[4], colors[6], 3700);
+    f[24] = getFireWorkBrightDarkBright(polarToCartesian([radius, 8 , 40]), colors[4], colors[6], 1200);
+    f[25] = getFireWorkBrightDarkBright(polarToCartesian([radius, 10 , 20]), colors[4], colors[6], 1800);
     f[26] = getFireWorkBrightDarkBright(polarToCartesian([radius, 35 , 33]), colors[4], colors[6], 200);
     f[27] = getFireWorkBrightDarkBright(polarToCartesian([radius, 65 , 40]), colors[4], colors[6], 3720);
     f[28] = getFireWorkBrightDarkBright(polarToCartesian([radius, 90 , 25]), colors[4], colors[6], 1020);
@@ -216,8 +216,8 @@
 
              quad={{
                     source:require("../res/particle_firework.png"),                 // Image source of the quad particle.
-                    height:0.01,
-                    width:0.01,
+                    height:0.02,
+                    width:0.02,
                     bloomThreshold:0.0
              }}
 
@@ -225,7 +225,7 @@
                particleLifetime:[1400,1500],
                emissionRatePerSecond:[0,0],
                emissionBurst:[
-                 {time:delay, min:500, max:500, cycles:10, cooldownPeriod:1200}
+                 {time:delay, min:500, max:500, cycles:10, cooldownPeriod:2300}
                ],
                spawnVolume:{shape:"sphere", params:[0.15], spawnOnSurface:true},
                maxParticles:800
@@ -268,8 +268,8 @@
 
                quad={{
                       source:require("../res/particle_firework.png"),                 // Image source of the quad particle.
-                      height:0.01,
-                      width:0.01,
+                      height:0.02,
+                      width:0.02,
                       bloomThreshold:0.0
                }}
 
@@ -277,7 +277,7 @@
                  particleLifetime:[1200,1200],
                  emissionRatePerSecond:[0,0],
                  emissionBurst:[
-                   {time:delay, min:500, max:500, cycles:10, cooldownPeriod:1400}
+                   {time:delay, min:500, max:500, cycles:10, cooldownPeriod:2500}
                  ],
                  spawnVolume:{shape:"sphere", params:[0.15], spawnOnSurface:true},
                  maxParticles:800
@@ -325,8 +325,8 @@
 
              quad={{
                     source:require("../res/particle_firework.png"),                 // Image source of the quad particle.
-                    height:0.01,
-                    width:0.01,
+                    height:0.02,
+                    width:0.02,
                     bloomThreshold:0.0
              }}
 
@@ -334,7 +334,7 @@
                particleLifetime:[600,600],
                emissionRatePerSecond:[0,0],
                emissionBurst:[
-                 {time:delay, min:400, max:500, cycles:2, cooldownPeriod:1550}
+                 {time:delay, min:400, max:500, cycles:2, cooldownPeriod:2050}
                ],
                spawnVolume:{shape:"sphere", params:[0.15], spawnOnSurface:true},
                maxParticles:800
