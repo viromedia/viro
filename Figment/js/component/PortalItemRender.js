6a122d7f1f40376da6f30d7f89582603853172bd
Daniel Thian <daniel@viromedia.com>
2017-09-05 16:00:13 -0700

    Fixed Shadows for Portals
    Similiar to the previous commit, but for portals instead.
    
    
    Former-commit-id: 78408d8ad2bce49ea324c3897b02b17ac9331b28

--- Figment/js/component/PortalItemRender.js
+++ Figment/js/component/PortalItemRender.js
@@ -111,7 +111,7 @@ var PortalItemRender = React.createClass({
 
           <ViroSurface
             rotation={[-90, 0, 0]}
-            position={[0, -.001 * this.props.bitMask, 0]}
+            position={[0, -.001, 0]}
             width={2.5} height={2.5}
             lightBitMask={this.props.bitMask | 1}
             materials={"shadowCatcher"}
