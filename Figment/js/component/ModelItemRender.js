49b0c65ebd920983e21807aa482fadb336495a43
Daniel Thian <daniel@viromedia.com>
2017-09-05 16:26:00 -0700

    Enlarged Shadow Surfaces to encompass Spherical shapes
    Former-commit-id: 159a8e7a95a9a9faf44c3d86e103211bd301700c

--- Figment/js/component/ModelItemRender.js
+++ Figment/js/component/ModelItemRender.js
@@ -103,7 +103,7 @@ var ModelItemRender = React.createClass({
             <ViroSurface
               rotation={[-90, 0, 0]}
               position={[0, -.001, 0]}
-              width={2} height={2}
+              width={2.5} height={2.5}
               lightBitMask={this.props.bitMask | 1}
               materials={"shadowCatcher"}
               acceptShadows={true}
