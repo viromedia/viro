9a16acde0f23bee0127e2721fa154a46d586a2aa
Vik Advani <vadvani@viromedia.com>
2017-09-13 17:42:56 -0700

    VIRO-1706: Weird animation behavior.
    Removed animation code that added random number to position, enabling pausing to work again.
    
    
    Former-commit-id: c3f949094ab0b8d4d13ce8811975515771e48bd6

--- components/ViroFlexView.js
+++ components/ViroFlexView.js
@@ -108,12 +108,6 @@ var ViroFlexView = React.createClass({
     onCollision: React.PropTypes.func,
   },
 
-  getInitialState: function() {
-    return {
-      propsPositionState:this.props.position,
-      nativePositionState:undefined
-    }
-  },
 
   _onHover: function(event: Event) {
     this.props.onHover && this.props.onHover(event.nativeEvent.isHovering, event.nativeEvent.position, event.nativeEvent.source);
@@ -193,35 +187,9 @@ var ViroFlexView = React.createClass({
   // for the underlying control within the renderer.
   _onNativeTransformUpdate: function(event: Event){
     var position =  event.nativeEvent.position;
-    this.setState({
-      nativePositionState:position
-    }, () => {
       if (this.props.onTransformUpdate){
         this.props.onTransformUpdate(position);
       }
-    });
-  },
-
-  // Set the propsPositionState on the native control if the
-  // nextProps.position state differs from the nativePositionState that
-  // reflects this control's current vroNode position.
-  componentWillReceiveProps(nextProps){
-    if(nextProps.position != this.state.nativePositionState){
-      var newPosition = [nextProps.position[0], nextProps.position[1], nextProps.position[2], Math.random()];
-      this.setState({
-        propsPositionState:newPosition
-      });
-    }
-  },
-
-  // Ignore all changes in native position state as it is only required to
-  // keep track of the latest position prop set on this control.
-  shouldComponentUpdate: function(nextProps, nextState) {
-    if (nextState.nativePositionState != this.state.nativePositionState){
-      return false;
-    }
-
-    return true;
   },
 
   setNativeProps: function(nativeProps) {
@@ -245,7 +213,6 @@ var ViroFlexView = React.createClass({
 
     let transformDelegate = this.props.onTransformUpdate != undefined ? this._onNativeTransformUpdate : undefined;
     let nativeProps = Object.assign({}, this.props);
-    nativeProps.position = this.state.propsPositionState;
     nativeProps.onNativeTransformDelegateViro = transformDelegate;
     nativeProps.hasTransformDelegate =this.props.onTransformUpdate != undefined;
     nativeProps.materials = materials;
