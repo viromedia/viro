ec8250f632e729164c9bc7e363254b8248be56f4
Andy Chu <andychu@viromedia.com>
2017-09-14 16:53:02 -0700

    Add Animation Properties to ViroFlexView
    Former-commit-id: 1786ac11a923852bb7c79e023258a890031c3141

--- components/ViroFlexView.js
+++ components/ViroFlexView.js
@@ -53,6 +53,14 @@ var ViroFlexView = React.createClass({
       PropTypes.arrayOf(PropTypes.string),
       PropTypes.string
     ]),
+    animation: PropTypes.shape({
+      name: PropTypes.string,
+      delay: PropTypes.number,
+      loop: PropTypes.bool,
+      onStart: PropTypes.func,
+      onFinish: PropTypes.func,
+      run: PropTypes.bool,
+    }),
     transformBehaviors: PropTypes.oneOfType([
       PropTypes.arrayOf(PropTypes.string),
       PropTypes.string
@@ -160,6 +168,14 @@ var ViroFlexView = React.createClass({
     }
   },
 
+  _onAnimationStart: function(event: Event) {
+    this.props.animation && this.props.animation.onStart && this.props.animation.onStart();
+  },
+
+  _onAnimationFinish: function(event: Event) {
+    this.props.animation && this.props.animation.onFinish && this.props.animation.onFinish();
+  },
+
   async getTransformAsync() {
     return await NativeModules.VRTNodeModule.getNodeTransform(findNodeHandle(this));
   },
@@ -236,6 +252,8 @@ var ViroFlexView = React.createClass({
     nativeProps.canFuse = this.props.onFuse != undefined;
     nativeProps.onFuseViro = this._onFuse;
     nativeProps.timeToFuse = timeToFuse;
+    nativeProps.onAnimationStartViro = this._onAnimationStart;
+    nativeProps.onAnimationFinishViro = this._onAnimationFinish;
     nativeProps.canCollide = this.props.onCollision != undefined;
     nativeProps.onCollisionViro = this._onCollision;
     nativeProps.ref = component => {this._component = component; };
@@ -271,7 +289,9 @@ var VROFlexView = requireNativeComponent(
             canCollide:true,
             onCollisionViro:true,
             onNativeTransformDelegateViro:true,
-            hasTransformDelegate:true
+            hasTransformDelegate:true,
+            onAnimationStartViro:true,
+            onAnimationFinishViro:true
           }
   }
 );
