8167e143f153779a06549408d3bf37dac5196459
Andy Chu <andychu@viromedia.com>
2017-09-15 18:10:49 -0700

    Add HelloWorld for AR and ExperienceSelector for iOS
    Former-commit-id: 28ba42efb23690f319dc934c0e3f5ed8e2ad4a77

--- bin/files/javascript/index.ios.js
+++ bin/files/javascript/index.ios.js
@@ -1,36 +1,165 @@
 /**
- * Copyright (c) 2016-present, Viro, Inc.
+ * Copyright (c) 2017-present, Viro, Inc.
  * All rights reserved.
  *
  * This source code is licensed under the BSD-style license found in the
  * LICENSE file in the root directory of this source tree. An additional grant
  * of patent rights can be found in the PATENTS file in the same directory.
  */
+
 import React, { Component } from 'react';
 import {
   AppRegistry,
-  StyleSheet,
   Text,
-  View
+  View,
+  StyleSheet,
+  PixelRatio,
+  TouchableHighlight,
 } from 'react-native';
 
 import {
   ViroSceneNavigator,
-  ViroScene,
-  ViroBox,
+  ViroARSceneNavigator
 } from 'react-viro';
 
-var InitialScene = require('./js/HelloWorldScene');
+/*
+ TODO: Insert your API key below
+ */
+var sharedProps = {
+  apiKey:"API_KEY_HERE",
+}
+
+// Sets the default scene you want for AR and VR
+var InitialARScene = require('./js/HelloWorldSceneAR');
+var InitialVRScene = require('./js/HelloWorldScene');
+
+var UNSET = "UNSET";
+var VR_NAVIGATOR_TYPE = "VR";
+var AR_NAVIGATOR_TYPE = "AR";
+
+// This determines which type of experience to launch in, or UNSET, if the user should
+// be presented with a choice of AR or VR. By default, we offer the user a choice.
+var defaultNavigatorType = UNSET;
 
 export default class ViroSample extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      navigatorType : defaultNavigatorType,
+      sharedProps : sharedProps
+    }
+    this._getExperienceSelector = this._getExperienceSelector.bind(this);
+    this._getARNavigator = this._getARNavigator.bind(this);
+    this._getVRNavigator = this._getVRNavigator.bind(this);
+    this._getExperienceButtonOnPress = this._getExperienceButtonOnPress.bind(this);
+  }
+
+  // Replace this function with the contents of _getVRNavigator() or _getARNavigator()
+  // if you are building a specific type of experience.
   render() {
+    if (this.state.navigatorType == UNSET) {
+      return this._getExperienceSelector();
+    } else if (this.state.navigatorType == VR_NAVIGATOR_TYPE) {
+      return this._getVRNavigator();
+    } else if (this.state.navigatorType == AR_NAVIGATOR_TYPE) {
+      return this._getARNavigator();
+    }
+  }
+
+  // Presents the user with a choice of an AR or VR experience
+  _getExperienceSelector() {
     return (
-      <ViroSceneNavigator apiKey="API_KEY_HERE"
-       initialScene={{scene: InitialScene}}
-        />
+      <View style={localStyles.outer} >
+        <View style={localStyles.inner} >
+
+          <Text style={localStyles.titleText}>
+            Choose your desired experience:
+          </Text>
+
+          <TouchableHighlight style={localStyles.buttons}
+            onPress={this._getExperienceButtonOnPress(AR_NAVIGATOR_TYPE)}
+            underlayColor={'#68a0ff'} >
+
+            <Text style={localStyles.buttonText}>AR</Text>
+          </TouchableHighlight>
+
+          <TouchableHighlight style={localStyles.buttons}
+            onPress={this._getExperienceButtonOnPress(VR_NAVIGATOR_TYPE)}
+            underlayColor={'#68a0ff'} >
+
+            <Text style={localStyles.buttonText}>VR</Text>
+          </TouchableHighlight>
+        </View>
+      </View>
     );
   }
+
+  // Returns the ViroARSceneNavigator which will start the AR experience
+  _getARNavigator() {
+    return (
+      <ViroARSceneNavigator {...this.state.sharedProps}
+        initialScene={{scene: InitialARScene}} />
+    );
+  }
+  
+  // Returns the ViroSceneNavigator which will start the VR experience
+  _getVRNavigator() {
+    return (
+      <ViroSceneNavigator {...this.state.sharedProps}
+        initialScene={{scene: InitialVRScene}} />
+    );
+  }
+
+  // This function returns an anonymous/lambda function to be used
+  // by the experience selector buttons
+  _getExperienceButtonOnPress(navigatorType) {
+    return () => {
+      this.setState({
+        navigatorType : navigatorType
+      })
+    }
+  }
 }
+
+var localStyles = StyleSheet.create({
+  outer : {
+    flex : 1,
+    flexDirection: 'row',
+    alignItems:'center',
+    backgroundColor: "black",
+  },
+  inner: {
+    flex : 1,
+    flexDirection: 'column',
+    alignItems:'center',
+    backgroundColor: "black",
+  },
+  titleText: {
+    paddingTop: 30,
+    paddingBottom: 20,
+    color:'#fff',
+    textAlign:'center',
+    fontSize : 25
+  },
+  buttonText: {
+    color:'#fff',
+    textAlign:'center',
+    fontSize : 20
+  },
+  buttons : {
+    height: 80,
+    width: 150,
+    paddingTop:20,
+    paddingBottom:20,
+    marginTop: 10,
+    marginBottom: 10,
+    backgroundColor:'#68a0cf',
+    borderRadius: 10,
+    borderWidth: 1,
+    borderColor: '#fff',
+  }
+});
 
 AppRegistry.registerComponent('APP_NAME_HERE', () => ViroSample);
 
---
+++ bin/files/javascript/js/HelloWorldSceneAR.js
@@ -0,0 +1,39 @@
+'use strict';
+
+import React, { Component } from 'react';
+
+import {StyleSheet} from 'react-native';
+
+import {
+  ViroARScene,
+  ViroText,
+  Viro360Image,
+} from 'react-viro';
+
+var HelloWorldSceneAR = React.createClass({
+  getInitialState() {
+    return {
+      text : "Initializing AR..."
+    };
+  },
+  render: function() {
+    return (
+     <ViroARScene onTrackingInitialized={()=>{this.setState({text : "Hello World!"})}}>
+       <ViroText text={this.state.text} scale={[.5, .5, .5]} position={[0, 0, -1]} style={styles.helloWorldTextStyle} />
+     </ViroARScene>
+    );
+  },
+});
+
+var styles = StyleSheet.create({
+  helloWorldTextStyle: {
+    fontFamily: 'Arial',
+    fontSize: 30,
+    color: '#ffffff',
+    textAlignVertical: 'center',
+    textAlign: 'center',  
+  },
+});
+
+module.exports = HelloWorldSceneAR;
+
