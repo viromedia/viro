9a67458263bfaf28e1d1a6f6c2a16ebffcd4a97f
Andy Chu <andychu@viromedia.com>
2018-03-27 11:47:08 -0700

    Remove the beta Renderer for iOS 11.3 release
    Former-commit-id: 365067a172283dd800527138d68a0ad28df671de

--- ios/dist/beta/ViroRenderer/ViroKit.framework/particles_fsh.glsl
+++
@@ -1,4 +0,0 @@
-// Grouped in 4N slots, should match VROParticlesUBOFragmentData structure defined in VROParticleUBO.h
-layout(std140) uniform particles_fragment_data{
-   mediump vec4 particles_fragment_color[180];
-};
