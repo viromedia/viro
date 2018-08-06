3793532c1567d9233825a31cdae4c59347b560a6
Manish Bodhankar <manish@viromedia.com>
2018-08-06 14:02:41 -0700

    Removing built intermediates from android samples

--- android/app/build/intermediates/assets/gvr/debug/constant_functions_fsh.glsl
+++
@@ -1,4 +0,0 @@
-#extension GL_OES_EGL_image_external : enable
-#extension GL_OES_EGL_image_external_essl3 : enable
-
-#include surface_functions_fsh
--- android/app/build/intermediates/exploded-aar/ViroSample/viro_renderer/unspecified/assets/particles_vsh.glsl
+++
@@ -1,4 +0,0 @@
-// Grouped in 4N slots, should match VROParticlesUBOVertexData structure defined in VROParticleUBO.h
-layout(std140) uniform particles_vertex_data{
-   mat4 particles_vertex_transform[180];
-};
