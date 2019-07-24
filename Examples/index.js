6c04fab5f5ea9a96803bcf2b612180ee33fec834
Vik Advani <vadvani@viromedia.com>
2019-07-24 13:07:37 -0700

    Modified release tests to have 2d list view.
    Former-commit-id: 36ac02a1ff065e98399eb5884f3aa285a8b650ae

--- Examples/index.js
+++ Examples/index.js
@@ -17,6 +17,7 @@ import {
   PixelRatio,
   TouchableHighlight,
   NativeModules,
+  SectionList,
 } from 'react-native';
 
 import {
@@ -39,7 +40,115 @@ var SCENE_NAVIGATOR_TYPE = "3D Scene";
 var VR_DEPRECATED_TYPE = "VR (deprecated)";
 
 var isARSupportedOnDevice = ViroUtils.isARSupportedOnDevice;
+
+var ARTests = [
+  { name: 'ARCameraHUDTest', scene: require('./js/AR/release_test/ARCameraHUDTest') },
+  { name: 'ARDragTest', scene: require('./js/AR/release_test/ARDragTest') },
+  { name: 'ARFBXVersionTest', scene: require('./js/AR/release_test/ARFBXVersionTest') },
+  { name: 'ARHitTest', scene: require('./js/AR/release_test/ARHitTest') },
+  { name: 'ARHitTestCamera', scene: require('./js/AR/release_test/ARHitTestCamera') },
+  { name: 'ARImageMarkerTest', scene: require('./js/AR/release_test/ARImageMarkerTest') },
+  { name: 'ARImageTrackingTest', scene: require('./js/AR/release_test/ARImageTrackingTest') },
+  { name: 'ARObjectMarkerTest', scene: require('./js/AR/release_test/ARObjectMarkerTest') },
+  { name: 'ARPlaneManualTest', scene: require('./js/AR/release_test/ARPlaneManualTest') },
+  { name: 'ARPlaneSelectorTest', scene: require('./js/AR/release_test/ARPlaneSelectorTest') },
+  { name: 'ARPlaneTest', scene: require('./js/AR/release_test/ARPlaneTest') },
+  { name: 'ARPolygonPlaneTest', scene: require('./js/AR/release_test/ARPolygonPlaneTest') },
+  { name: 'ARSceneAndNavigatorTest', scene: require('./js/AR/release_test/ARSceneAndNavigatorTest') },
+  { name: 'ARShadowTest', scene: require('./js/AR/release_test/ARShadowTest') },
+  { name: 'ARPbrTest', scene: require('./js/AR/release_test/ViroPbrTestAR') },
+  { name: 'ARPbrTestSurfacesAR', scene: require('./js/AR/release_test/ViroPbrTestSurfacesAR') },
+  { name: 'ARPostProcessTest', scene: require('./js/AR/release_test/ViroPostProcessTestAR') },
+];
+var VRTests = [
+  { name: 'GroupTestBasicAnimation', scene: require('./js/release_test/GroupTestBasicAnimation') },
+  { name: 'GroupTestBasicBillboard', scene: require('./js/release_test/GroupTestBasicBillboard') },
+  { name: 'GroupTestBasicEvents', scene: require('./js/release_test/GroupTestBasicEvents') },
+  { name: 'GroupTestBasicProps', scene: require('./js/release_test/GroupTestBasicProps') },
+  { name: 'GroupTestDragEvents', scene: require('./js/release_test/GroupTestDragEvents') },
+  { name: 'GroupTestMaterials', scene: require('./js/release_test/GroupTestMaterials') },
+  { name: 'PhysicsCollisionTest', scene: require('./js/release_test/PhysicsCollisionTest') },
+  { name: 'PhysicsForcesTest', scene: require('./js/release_test/PhysicsForcesTest') },
+  { name: 'PhysicsGroupTest', scene: require('./js/release_test/PhysicsGroupTest') },
+  { name: 'PhysicsPropertiesTest', scene: require('./js/release_test/PhysicsPropertiesTest') },
+  { name: 'PhysicsScaleTest', scene: require('./js/release_test/PhysicsScaleTest') },
+  { name: '3DObjectTest', scene: require('./js/release_test/Viro3DObjectTest') },
+  { name: '360ImageTest', scene: require('./js/release_test/Viro360ImageTest') },
+  { name: '360VideoTest', scene: require('./js/release_test/Viro360VideoTest') },
+  { name: '360VideoTest', scene: require('./js/release_test/Viro360VideoTest') },
+  { name: 'AnimatedComponentTest', scene: require('./js/release_test/ViroAnimatedComponentTest') },
+  { name: 'AnimatedImageTest', scene: require('./js/release_test/ViroAnimatedImageTest') },
+  { name: 'AnimationTest', scene: require('./js/release_test/ViroAnimationTest') },
+  { name: 'BlendingTest', scene: require('./js/release_test/ViroBlendingTest') },
+  { name: 'BoxTest', scene: require('./js/release_test/ViroBoxTest') },
+  { name: 'ButtonTest', scene: require('./js/release_test/ViroButtonTest') },
+  { name: 'CameraHUDTest', scene: require('./js/release_test/ViroCameraHUDTest') },
+  { name: 'CameraTest', scene: require('./js/release_test/ViroCameraTest') },
+  { name: 'ChromaKeyTest', scene: require('./js/release_test/ViroChromaKeyTest') },
+  { name: 'ColorMaskTest', scene: require('./js/release_test/ViroColorMaskTest') },
+  { name: 'FlexViewLayoutTest', scene: require('./js/release_test/ViroFlexViewLayoutTest') },
+  { name: 'FontTest', scene: require('./js/release_test/ViroFontTextTest') },
+  { name: 'GeometryTest', scene: require('./js/release_test/ViroGeometryTest') },
+  { name: 'GLTFTest', scene: require('./js/release_test/ViroGLTFTest') },
+  { name: 'HierarchyTest', scene: require('./js/release_test/ViroHierarchyTest') },
+  { name: 'ImageTest', scene: require('./js/release_test/ViroImageTest') },
+  { name: 'InternationalTextTest', scene: require('./js/release_test/ViroInternationalTextTest') },
+  { name: 'LightTest', scene: require('./js/release_test/ViroLightTest') },
+  { name: 'MaterialVideoTest', scene: require('./js/release_test/ViroMaterialVideoTest') },
+  { name: 'ParticleTest', scene: require('./js/release_test/ViroParticleTest') },
+  { name: 'PbrBallTest', scene: require('./js/release_test/ViroPbrBallTest') },
+  { name: 'PbrTest', scene: require('./js/release_test/ViroPbrTest') },
+  { name: 'PbrTestSurfaces', scene: require('./js/release_test/ViroPbrTestSurfaces') },
+  { name: 'PivotTest', scene: require('./js/release_test/ViroPivotTest') },
+  { name: 'PolygonTest', scene: require('./js/release_test/ViroPolygonTest') },
+  { name: 'PolylineTest', scene: require('./js/release_test/ViroPolylineTest') },
+  { name: 'PortalTest', scene: require('./js/release_test/ViroPortalTest') },
+  { name: 'PortalTestVR', scene: require('./js/release_test/ViroPortalTestVR') },
+  { name: 'QuadFlexViewTest', scene: require('./js/release_test/ViroQuadFlexViewTest') },
+  { name: 'ShadowGroupTest', scene: require('./js/release_test/ViroShadowGroupTest') },
+  { name: 'ShadowTest', scene: require('./js/release_test/ViroShadowTest') },
+  { name: 'SkyBoxTest', scene: require('./js/release_test/ViroSkyBoxTest') },
+  { name: 'SoundTest', scene: require('./js/release_test/ViroSoundTest') },
+  { name: 'TextSpecialTest', scene: require('./js/release_test/ViroTextSpecialTest') },
+  { name: 'TextTest', scene: require('./js/release_test/ViroTextTest') },
+  { name: 'VideoTest', scene: require('./js/release_test/ViroVideoTest') },
+];
+var SceneTests = [
+  { name: 'GroupTestBasicEvents', scene: require('./js/release_test/GroupTestBasicEvents')},
+  { name: 'GroupTestDragEvents', scene:  require('./js/release_test/GroupTestDragEvents')},
+  { name: 'VideoTest', scene: require('./js/release_test/ViroVideoTest')},
+  { name: 'SoundTest', scene: require('./js/release_test/ViroSoundTest')},
+];
+
+
 export default class ViroExperienceSelector extends Component {
+
+GetSectionListItem = (section, item) => {
+  //Function for click on an item
+  if (section.title == "AR Tests") {
+    this.setState({navigatorType : AR_NAVIGATOR_TYPE,
+                   initialScene:item,
+    });
+  } else if (section.title == "VR Tests") {
+    this.setState({navigatorType : VR_NAVIGATOR_TYPE,
+                  initialScene: item,
+    });
+  } else if (section.title == "Scene Tests"){
+    this.setState({navigatorType : SCENE_NAVIGATOR_TYPE,
+                   initialScene: item,
+    });
+  }
+};
+FlatListItemSeparator = () => {
+  return (
+    //Item Separator
+    <View
+      style={{ height: 0.5, width: '100%', backgroundColor: '#C8C8C8' }}
+    />
+  );
+};
+
+
   constructor() {
     super();
 
@@ -56,6 +165,7 @@ export default class ViroExperienceSelector extends Component {
       videoQualityHigh : true,
       initial3DScene : UNSET,
       numberOfTrackedImages: 0,
+      initialScene: null,
     }
     this._getSelectionButtons = this._getSelectionButtons.bind(this);
     this._getARNavigator = this._getARNavigator.bind(this);
@@ -70,9 +180,11 @@ export default class ViroExperienceSelector extends Component {
     this._getInitialSelectionButtons = this._getInitialSelectionButtons.bind(this);
     this._getSceneSelectionButtons = this._getSceneSelectionButtons.bind(this);
   }
+
   componentDidMount() {
     isARSupportedOnDevice(this._handleARNotSupported,this._handleARSupported);
   }
+
   _handleARSupported() {
     console.log("AR supported");
   }
@@ -89,64 +201,41 @@ export default class ViroExperienceSelector extends Component {
         return this._getVRNavigator();
       }
     } else if (this.state.navigatorType == AR_NAVIGATOR_TYPE) {
-      if (this.state.worldAlignment == UNSET) {
-        return this._getSelectionButtons();
-      } else {
         return this._getARNavigator();
-      }
     } else if (this.state.navigatorType == SCENE_NAVIGATOR_TYPE ) {
-      if (this.state.initial3DScene == UNSET) {
-        return this._getInitialSelectionButtons();
-      } else {
-        return this._get3DSceneNavigator();
-      }
+      //if (this.state.initial3DScene == UNSET) {
+      //  return this._getInitialSelectionButtons();
+      // } else {
+      //  return this._get3DSceneNavigator();
+      // }
     }
   }
     _getSceneSelectionButtons() {
     return (
       <View style={localStyles.outer} >
-        <View style={localStyles.inner} >
-
-          <Text style={localStyles.titleText}>
-            {this._getTitleText()}
-          </Text>
-
-          <TouchableHighlight style={this.state.navigatorType == UNSET ? localStyles.buttons : localStyles.vrModeButtons}
-            onPress={this._getButtonPress(1)}
-            underlayColor={'#68a0ff'} >
-
-            <Text style={localStyles.buttonText}>
-              {this._getButtonText(1)}
-            </Text>
-          </TouchableHighlight>
-
-          <TouchableHighlight style={this.state.navigatorType == UNSET ? localStyles.buttons : localStyles.vrModeButtons}
-            onPress={this._getButtonPress(2)}
-            underlayColor={'#68a0ff'} >
-
-            <Text style={localStyles.buttonText}>
-              {this._getButtonText(2)}
-            </Text>
-          </TouchableHighlight>
-
-          <TouchableHighlight style={this.state.navigatorType == UNSET ? localStyles.buttons : localStyles.vrModeButtons}
-            onPress={this._getButtonPress(3)}
-            underlayColor={'#68a0ff'} >
-
-            <Text style={localStyles.buttonText}>
-              {this._getButtonText(3)}
-            </Text>
-          </TouchableHighlight>
-
-          <TouchableHighlight style={this.state.navigatorType == UNSET ? localStyles.buttons : localStyles.vrModeButtons}
-            onPress={this._getButtonPress(4)}
-            underlayColor={'#68a0ff'} >
-
-            <Text style={localStyles.buttonText}>
-              {this._getButtonText(4)}
-            </Text>
-          </TouchableHighlight>
-        </View>
+          <SectionList
+            ItemSeparatorComponent={this.FlatListItemSeparator}
+            sections={[
+              { title: 'AR Tests', data: ARTests },
+              { title: 'VR Tests', data: VRTests },
+              { title: 'Scene Tests', data: SceneTests },
+            ]}
+            renderSectionHeader={({ section }) => (
+              <Text style={localStyles.sectionHeaderStyle}> {section.title} </Text>
+            )}
+            renderItem={({ item, section }) => (
+              // Single Comes here which will be repeatative for the FlatListItems
+              <Text
+                style={localStyles.sectionListItemStyle}
+                //Item Separator View
+                onPress={this.GetSectionListItem.bind(
+                  this, section, item.scene
+                )}>
+                {item.name}
+              </Text>
+            )}
+            keyExtractor={(item, index) => index}
+          />
       </View>
     );
   }
@@ -268,8 +357,6 @@ export default class ViroExperienceSelector extends Component {
           case 4:
             return "Sounds";
         }
-      }else {
-        return (buttonNum == 1 ? "Gravity" : "Heading");
       }
     }
   }
@@ -278,10 +365,10 @@ export default class ViroExperienceSelector extends Component {
       <View style={localStyles.viroContainer} >
         <ViroARSceneNavigator
           {...this.state.sharedProps}
-          initialScene={{scene: InitialARScene}}
+          initialScene={{scene: this.state.initialScene}}
           multisamplingEnabled={false}
           numberOfTrackedImages={this.state.numberOfTrackedImages}
-          worldAlignment={this.state.worldAlignment}
+          worldAlignment={"Gravity"}
           videoQuality={this.state.videoQualityHigh ? "High" : "Low"}
           autofocus={this.state.autofocus}
           viroAppProps={{ toggleAutofocus : this._toggleAutofocus,
@@ -305,12 +392,12 @@ export default class ViroExperienceSelector extends Component {
       <View style={localStyles.viroContainer} >
         {renderIf(this.state.navigatorType == VR_NAVIGATOR_TYPE,
           <ViroVRSceneNavigator {...this.state.sharedProps}
-            initialScene={{scene: InitialVRScene}}
+            initialScene={{scene: this.state.initialScene}}
             vrModeEnabled={this.state.vrMode}
             onExitViro={()=>{this.setState({navigatorType : UNSET, vrMode : UNSET})}}/>)}
         {renderIf(this.state.navigatorType == VR_DEPRECATED_TYPE,
           <ViroSceneNavigator {...this.state.sharedProps}
-            initialScene={{scene: InitialVRScene}}
+            initialScene={{scene: this.state.initialScene}}
             vrModeEnabled={this.state.vrMode}
             onExitViro={()=>{this.setState({navigatorType : UNSET, vrMode : UNSET})}}/>)}
 
@@ -330,7 +417,7 @@ export default class ViroExperienceSelector extends Component {
       <View style={localStyles.viroContainer} >
         <Viro3DSceneNavigator
           {...this.state.sharedProps}
-          initialScene={{scene: this.state.initial3DScene == UNSET ? InitialVRScene : this.state.initial3DScene}}
+          initialScene={{scene: this.state.initialScene}}
           onExitViro={()=>{this.setState({navigatorType : UNSET, vrMode : UNSET, initial3DScene: UNSET})}}/>
 
         <View style={localStyles.exitButtonContainer}>
@@ -467,7 +554,19 @@ var localStyles = StyleSheet.create({
     borderRadius: 10,
     borderWidth: 1,
     borderColor: '#fff',
-  }
+  },
+  sectionHeaderStyle: {
+    backgroundColor: '#6699CC',
+    fontSize: 20,
+    padding: 5,
+    color: '#fff',
+  },
+  sectionListItemStyle: {
+    fontSize: 15,
+    padding: 15,
+    color: '#000',
+    backgroundColor: "#FFF",
+  },
 });
 
 AppRegistry.registerComponent('ViroSample', () => ViroExperienceSelector);
