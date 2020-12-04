5fbd533e2d932c58e6894ef3901091ab1bcd9916
Stefanos Markidis <stmarkidis@gmail.com>
2020-12-04 18:46:20 +0100

    Fix android sourceDir

--- react-native.config.js
+++ react-native.config.js
@@ -2,7 +2,7 @@ module.exports = {
   dependency: {
     platforms: {
       android: {
-        sourceDir: "./android/viro_bridge",
+        sourceDir: "./android",
         packageImportPath: "import com.viromedia.bridge.ReactViroPackage;",
         packageInstance:
           "new ReactViroPackage(ReactViroPackage.ViroPlatform.AR)",
