923da1c6852d45a2542cd932e3e0d6d4db6e6c35
Stefanos Markidis <stmarkidis@gmail.com>
2020-12-04 19:23:20 +0100

    Include viro renderer as libs dependency

--- react-native.config.js
+++ react-native.config.js
@@ -2,7 +2,7 @@ module.exports = {
   dependency: {
     platforms: {
       android: {
-        sourceDir: "./android",
+        sourceDir: "./android/viro_bridge",
         packageImportPath: "import com.viromedia.bridge.ReactViroPackage;",
         packageInstance:
           "new ReactViroPackage(ReactViroPackage.ViroPlatform.AR)",
