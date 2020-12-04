90ad5a06e38dfee4222d4c96661138b6e4ac004f
Stefanos Markidis <stmarkidis@gmail.com>
2020-12-04 18:17:37 +0100

    Add top level podspec instead

--- react-native.config.js
+++ react-native.config.js
@@ -1,15 +1,12 @@
 module.exports = {
-    dependency: {
-      platforms: {
-        android: {
-            sourceDir: "./android/viro_bridge",
-            packageImportPath: "import com.viromedia.bridge.ReactViroPackage;",
-            packageInstance: "new ReactViroPackage(ReactViroPackage.ViroPlatform.AR)"
-        },
-        ios: {
-            folder:"./ios",
-            projectPath: "./ios/ViroReact.xcworkspace",
-        }
+  dependency: {
+    platforms: {
+      android: {
+        sourceDir: "./android/viro_bridge",
+        packageImportPath: "import com.viromedia.bridge.ReactViroPackage;",
+        packageInstance:
+          "new ReactViroPackage(ReactViroPackage.ViroPlatform.AR)",
       },
     },
-  };
+  },
+};
---
+++ react-viro.podspec
@@ -0,0 +1,19 @@
+require 'json'
+
+package = JSON.parse(File.read(File.join(__dir__, 'package.json')))
+
+Pod::Spec.new do |s|
+  s.name         = "react-iro"
+  s.version      = package['version']
+  s.summary      = "React Viro"
+
+  s.license      = "MIT"
+  s.platform     = :ios, "8.0"
+
+  s.source       = { :git => "https://github.com/mendix/viro.git", :branch=> "nalm/auto-linking" }
+  s.source_files  = ["ios/ViroReact/**/*.{h,m}", "ios/ViroReactFramework/**/*.{h,m}"]
+
+  s.dependency 'React-Core'
+  s.dependency 'GoogleMaps', '3.5.0'
+  s.dependency 'Google-Maps-iOS-Utils', '2.1.0'
+end
